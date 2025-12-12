5fbb34b3d664909a0865c4d95135d285e32bf65d
namph342 <maxsida42@gmail.com>
2025-12-12 20:45:24 +0700

    xay mong

---
+++ App/QLCC/index.py
@@ -0,0 +1,16 @@
+from flask import render_template
+
+from QLCC import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
